9b095d852a0b9d1020c890fa11a373a6ff0c6f40
Mazahir Hashimli <mazahir89@gmail.com>
2020-01-10 14:05:13 +0100

    created productlist component and its template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StarComponent } from './components/shared/star/star.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarComponent,
+    HomeComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
